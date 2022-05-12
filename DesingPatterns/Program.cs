eb119091062e612a85e435d3d68946559ab801fb
Pedro Kaleb! <73436445+LyeZinho@users.noreply.github.com>
2022-05-12 10:07:50 +0100

    Adicionar arquivos de projeto.

---
+++ DesingPatterns/Program.cs
@@ -0,0 +1,12 @@
+﻿using DesingPatterns;
+using System;
+using DesingPatterns.SimpleFactory;
+
+static class Program
+{
+    static void Main()
+    {
+        IPizza pizza = PizzaFactory.CriarPizza(TipoPizza.marguerita);
+        pizza.PrepararPizza();
+    }
+}
---
+++ DesingPatterns/SimpleFactory/SimpleFactory.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesingPatterns.SimpleFactory
+{
+    public enum TipoPizza
+    {
+        calabreza = 1,
+        marguerita  = 2
+    }
+
+    public interface IPizza
+    {
+        void PrepararPizza();
+    }
+
+    public class Calabreza : IPizza
+    {
+        public void PrepararPizza()
+        {
+            Console.WriteLine("Calabreza preparada...");   
+        }
+    }
+
+    public class Marguerita : IPizza
+    {
+        public void PrepararPizza()
+        {
+            Console.WriteLine("Marguerita preparada...");
+        }
+    }
+
+    public class PizzaFactory
+    {
+        public static IPizza CriarPizza(TipoPizza tipo)
+        {
+            switch (tipo)
+            {
+                case TipoPizza.calabreza:
+                    return new Calabreza();
+                case TipoPizza.marguerita:
+                    return new Marguerita();
+                default:
+                    throw new Exception("Tipo invalido!");
+            }
+        }
+    }
+}
