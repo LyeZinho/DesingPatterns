e99eba7690479d276cfaf9e3ad2ec27535e0e10d
Pedro Kaleb! <73436445+LyeZinho@users.noreply.github.com>
2022-05-13 18:07:17 +0100

    Update Program.cs

--- DesingPatterns/Program.cs
+++ DesingPatterns/Program.cs
@@ -6,24 +6,6 @@ static class Program
 {
     static void Main()
     {
-        /*
-        Cria um objeto calcula salario do tipo CalculaSalario
-        Passa como parametro uma instansia do tipo GeraJson que implementa a interface IGeraArquivo
-        */
-        CalculaSalario calculaSalario = new CalculaSalario(new GeraJson());
 
-        //Cria um objeto funcionario
-        Funcionario funcionario = new Funcionario()
-        {
-            Cargo = "Desenvolvedor",
-            Nome = "Fulano",
-            Departamento = "TI",
-            Salario = 1000,
-            Id = 1,
-            Incentivo = 30.0,
-        };
-
-        //Chama o metodo CalculaSalario e processa os dados de funcionario
-        calculaSalario.SalvarArquivo(funcionario);
     }
 }
